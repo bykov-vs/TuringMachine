08d9c2487780d845b0c8bc48148001356529f1af
FuryPotat0 <oleg.bakharev.00@mail.ru>
2022-12-25 15:47:48 +0400

    сделал отображение операндов на ленте

--- src/main/webapp/src/app/components/settings/settings.component.ts
+++ src/main/webapp/src/app/components/settings/settings.component.ts
@@ -2,6 +2,9 @@ import {Component, EventEmitter, Output, Input} from '@angular/core';
 import {Command} from '../alg-table/command';
 import {HttpService} from "../../HttpService";
 
+let tapeElements: Array<string> = [];
+
+
 @Component({
     selector: 'app-settings',
     templateUrl: './settings.component.html',
@@ -13,13 +16,11 @@ export class SettingsComponent {
     steps: any = []
     @Output() taleLengthChange = new EventEmitter<Event>()
 
-    tapeElements: Array<string> = [];
-
     operand1: Number = 0
     operand2: Number = 0
     mode: String = "standard"
     @Input() commands: Command[] = []
-    @Input() _symbols: String[] = []
+    @Input() symbols: String[] = []
 
     constructor(private httpService: HttpService) {
     }
@@ -30,7 +31,7 @@ export class SettingsComponent {
         let tapeLength: number = 3
         let firstOperand: String = this.operand1.toString()
         let secondOperator: String = this.operand2.toString()
-        for (let x of this._symbols) {
+        for (let x of this.symbols) {
             symbols.push({"name": x})
         }
         let alphabet = {"alphabet": symbols}
@@ -48,15 +49,31 @@ export class SettingsComponent {
         this.mode = mode
     }
 
+    onOperandChange() {
+        let tapeElement, i = 1;
+        while (tapeElement = document.getElementById('tape-element-' + i)){
+            if ((i >= 3 && i < 3+this.operand1.valueOf()) ||
+                (i >= 3+1+this.operand1.valueOf() && i < 3+1+this.operand1.valueOf()+this.operand2.valueOf())){
+                // @ts-ignore
+                tapeElement.value = this.symbols[0]
+            }
+            else {
+                // @ts-ignore
+                tapeElement.value = '_'
+            }
+            i++
+        }
+    }
+
     runAlgorithm() {
         let i = 1, tapeElement;
-        this.tapeElements = [];
+        tapeElements = [];
         while (tapeElement = document.getElementById('tape-element-' + i)){
             // @ts-ignore
             this.tapeElements.push(tapeElement.value)
             i++
         }
-        console.log("Элементы ленты" + this.tapeElements)
+        console.log("Элементы ленты" + tapeElements)
 
         let element, tapeScope, combobox
         (async () => {
